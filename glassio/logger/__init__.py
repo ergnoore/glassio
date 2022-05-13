195d6df77aeb98e40e7a025da06cb57b93b55d1c
Aleksandr Filippov <alexander.filippov@iqtek.ru>
2022-05-13 16:33:25 +0600

    Fixed __all__ in logger.core .

--- glassio/logger/__init__.py
+++ glassio/logger/__init__.py
@@ -3,8 +3,10 @@
 from .abstract import AbstractLogger
 from .core import ILogger
 from .core import ILoggerFactory
+from .core import InitializableLogger
 from .core import Level
 from .core import Message
+
 from .standart import get_initialized_logger
 from .standart import StandardLogger
 from .standart import StandardLoggerFactory
@@ -15,6 +17,7 @@ __all__ = [
     "Message",
     "ILogger",
     "ILoggerFactory",
+    "InitializableLogger",
     "StandardLogger",
     "StandardLoggerFactory",
     "get_initialized_logger",
